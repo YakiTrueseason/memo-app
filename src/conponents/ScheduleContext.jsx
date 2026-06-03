21aee414f26fae7cfd337c1701d07d69e6bde731
YakiTrueseason <254607726+YakiTrueseason@users.noreply.github.com>
2026-06-03 23:21:16 +0900

    feat:localstorage keep

--- src/conponents/ScheduleContext.jsx
+++ src/conponents/ScheduleContext.jsx
@@ -1,15 +1,31 @@
 //現在の選択中の日付一覧をまとめて保存
 
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ScheduleContext = createContext();
 
 export function ScheduleProvider({children}){
+    //日付を初期化
+    // const today = new Date().toISOString().split("T")[0];
 
-    const[selectedDate,setSelectedDate] = useState("2026-06-01");
+    const[selectedDate,setSelectedDate] = useState(()=>{
+        const savedDate = localStorage.getItem("selectedDate");
+        if(savedDate){
+            return savedDate;
+        }
+        return new Date()
+            .toISOString()
+            .split("T")[0];
+    });
 
     const[schedules,setSchedules] = useState([]);
 
+    useEffect(()=>{
+        localStorage.setItem(
+            "selectedDate",
+            selectedDate
+        );
+    },[selectedDate])
 return (
     <ScheduleContext.Provider
         value={{
